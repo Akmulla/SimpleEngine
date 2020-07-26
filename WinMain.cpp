f36322a9f317f0a0ee363352d6bc71d2a21f0833
Akmulla <d4n73@yandex.ru>
2020-07-26 03:39:05 +0300

    WindowCreation

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,11 +1,44 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
+	_In_ HINSTANCE hInstance,
+	_In_opt_  HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
+	const auto winClassName = "SimpleExampleWindowClass";
+
+	WNDCLASSEX wc = { };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = winClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		winClassName,
+		"MyWindow",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,
+		200,
+		640,
+		480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+	
+	ShowWindow(hwnd, SW_SHOW);
 	while (true);
 	return 0;
 }
