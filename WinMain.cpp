d20ac89e1e61eb6a259062e9175992654a379956
Akmulla <d4n73@yandex.ru>
2020-07-27 08:54:53 +0300

    Message loop

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(1);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_  HINSTANCE hPrevInstance,
@@ -11,7 +22,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -39,6 +50,21 @@ int CALLBACK WinMain(
 	);
 	
 	ShowWindow(hwnd, SW_SHOW);
-	while (true);
-	return 0;
+
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
